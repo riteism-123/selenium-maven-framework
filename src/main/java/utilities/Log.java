f2c0c0fc62b8a7aec48ce191a4ff19a4a582afd0
Ritesh <riteismx@gmail.com>
2025-04-27 18:20:24 +0530

    fixed workflows

--- src/main/java/utilities/Log.java
+++ src/main/java/utilities/Log.java
@@ -36,6 +36,8 @@ public class Log {
     public static void warn(String message) {
     	logger.warn(message);
     }
+
+	
 }
 
 
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,22 +1,87 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import utilities.DriverFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import utilities.PropertyUtils;
+import utilities.ExtentManager;
+import utilities.Log;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 
-    protected WebDriver driver;
+	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected static ExtentTest test;
+	private static final Logger logger = LogManager.getLogger(BaseTest.class);
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() {
-        driver = DriverFactory.getDriver();
-    }
+	@BeforeSuite
+	public void beforeSuite() {
+		extent = ExtentManager.getExtentReports();
+	}
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult result) {
-        DriverFactory.quitDriver();
-    }
+	@BeforeMethod
+	@Parameters("browser")
+	public void setUp(@Optional("chrome") String browserName) {
+		Log.info("Starting test setup");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--headless=new"); // optional if you want headless
+			driver = new ChromeDriver(options);
+
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			EdgeOptions options = new EdgeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--remote-allow-origins=*");
+			options.addArguments("--headless=new");
+			driver = new EdgeDriver(options);
+
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--headless=new");
+			driver = new FirefoxDriver(options);
+
+		} else {
+			throw new IllegalArgumentException("Invalid browser: " + browserName);
+		}
+
+		driver.manage().window().maximize();
+		//driver.get(PropertyUtils.getProperty("url"));
+
+		//Log.info("Browser launched and navigated to URL: " + ConfigReader.getProperty("url"));
+	}
+
+	@AfterMethod(alwaysRun = true)
+	public void tearDown(ITestResult result) {
+		DriverFactory.quitDriver();
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		if (extent != null) {
+			extent.flush();
+		}
+	}
 }
